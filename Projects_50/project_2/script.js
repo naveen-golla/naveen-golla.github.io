1b5e890a8f7abf767ddd6ec1644196a3d0d742e9
naveen-golla <naveen.g@volkotech.in>
2021-04-15 23:32:45 +0530

    updated Jqurey

--- Projects_50/project_2/script.js
+++ Projects_50/project_2/script.js
@@ -1,63 +1,39 @@
-const progress = document.getElementById("progress")
-const prev = document.getElementById("prev")
-const next = document.getElementById("next")
-const circles = document.querySelectorAll(".circle")
+var currentActive = 1;
 
-let currentActive = 1;
+$(document).ready(function() {
 
-next.addEventListener("click", () => {
-    currentActive++;
-
-    if (currentActive > circles.length) {
-        currentActive = circles.length
-    }
-    update()
-});
-
-prev.addEventListener("click", () => {
-    currentActive--;
-
-    if (currentActive < 1) {
-        currentActive = 1
-    }
-    update()
-});
-
-function update() {
-    circles.forEach((circle, index) => {
-        if (index < currentActive) {
-            circle.classList.add('active');
-        } else {
-            circle.classList.remove('active')
+    $("#next").click(function() {
+        currentActive++;
+        if (currentActive > $(".circle").length) {
+            currentActive = $(".circle").length
+        } else if (currentActive === $(".circle").length) {
+            $("#next").attr("disabled", "disabled");
         }
-    })
+        $("#prev").removeAttr("disabled");
+        update(currentActive)
+    });
+    // 
+    $("#prev").click(function() {
+        currentActive--;
+        if (currentActive < 1) {
+            currentActive = 1
+        } else if (currentActive == 1) {
+            $("#prev").attr("disabled", "disabled");
+        }
+        $("#next").removeAttr("disabled", "disabled");
+        update(currentActive)
+    });
+    const active = $('.active')
 
-    const actives = document.querySelectorAll('.active');
-
-    progress.style.width = (actives.length - 1) / (circles.length - 1) *
-        100 + "%";
-
-    if (currentActive === 1) {
-        prev.disabled = true
-    } else if (currentActive === circles.length) {
-        next.disabled = true
-    } else {
-        prev.disabled = false
-        next.disabled = false
+    function update(currentActive) {
+        $('.circle').each(function() {
+            if (parseInt($(this).html()) === currentActive) {
+                $(this).addClass("active");
+                console.log(currentActive)
+            } else if (parseInt($(this).html()) >= currentActive) {
+                $(this).removeClass("active");
+                console.log(currentActive)
+            }
+        });
     }
-};
-
-// var currentActive = 1
-
-// $(document).ready(function() {
-
-//     $("#next").click(function(circles) {
-
-//         currentActive++;
-//         if (currentActive > circles.length) {
-//             currentActive = circles.length
-//         }
-//     })
-
-
-// })
+})
