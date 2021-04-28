93d623d41f9137ebb605823ec8125e364f00f749
naveen-golla <naveen.g@volkotech.in>
2021-04-28 14:04:29 +0530

    Jquery fixes

--- Projects_50/project-8/script.js
+++ Projects_50/project-8/script.js
@@ -1,16 +1,17 @@
-const labels = document.querySelectorAll('.form-control label');
+// const labels = document.querySelectorAll('.form-control label');
 
-labels.forEach(label => {
-    label.innerHTML = label.innerText.split('')
-        .map((letter, index) => `<span style="transition-delay:${index * 50}ms">${letter}</span>`)
-        .join('');
-});
-
-// $(function() {
-//     $('.form-control label').each(function() {
-//         $('.label').html() == $('label').text().split('')
-//             .map(function(letter, index) {
-//                 `<span style="transition-delay:${index * 50}ms">${letter}</span>`
-//             }).join('');
-//     });
+// labels.forEach(label => {
+//     label.innerHTML = label.innerText
+//         .split('')
+//         .map((letter, index) => `<span style="transition-delay:${index * 50}ms">${letter}</span>`)
+//         .join('');
 // });
+
+$(function() {
+    $('.form-control label').each(function() {
+        this.innerHTML = this.innerText
+            .split('')
+            .map((letter, index) => `<span style="transition-delay:${index * 50}ms">${letter}</span>`)
+            .join('');
+    });
+});
